f15c214a153390e2c21e7d8d13f6d40fd18e5811
MarinBarac <marinbarac21@gmail.com>
2026-03-12 17:20:14 +0100

    solved exercises 0-16

---
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // TODO: Create an array called `a` with at least 100 elements in it.
+    // let a = ???
+
+    let mut a: Vec<u32> = vec![];
+
+    for i in 0..100 {
+        a.push(i);
+    }
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+        panic!("Array not big enough, more elements needed");
+    }
+}
